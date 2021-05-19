5e541bababd253fb73e2dc2076f088a5e537393c
Aniket Biswas <74890960+aniket-biswas@users.noreply.github.com>
2021-05-19 13:56:13 +0530

    Delete Chapter 2 _ Function/Homework directory

--- Chapter 2 _ Function/Homework/12. Write a Python function that checks whether a passed string is palindrome or not.py
+++
@@ -1,9 +0,0 @@
-def palindrome(): 
-  s=str(input("Enter string:"))
-  s=s.upper()
-  a=reversed(s)
-  if list(s)==list(a):
-    print("It's a Palindrome String")
-  else:
-    print("It's not a palindrome")
-palindrome()
--- Chapter 2 _ Function/Homework/2. Write a Python function to sum all the numbers in a list.py
+++
@@ -1,9 +0,0 @@
-def maxlist():
-    n=int(input("Enter the no of no's:"))
-    l=[]
-    for i in range(0,n):
-        element=int(input("Enter no:"))
-        l.append(element)
-        a=sum(l)
-    print(a)
-maxlist()
